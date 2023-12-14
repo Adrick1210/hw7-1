6dd10b6e967ff47c9423a29d6d36d41f94d770cd
Christian Alexander <christian.alexanderato@gmail.com>
2023-12-13 21:42:40 -0800

    tip calculator express app created

--- server.js
+++ server.js
@@ -2,9 +2,24 @@ const express = require("express");
 
 const app = express();
 
+//=================================
+// Greeting
+//=================================
+
 app.get("/greeting/:name", (req, res) => {
   const name = req.params.name;
   res.send(`<h1>Hello there, ${name}</h1>`);
+});
+
+//=================================
+// Calculator
+//=================================
+
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  const total = req.params.total;
+  const percent = req.params.tipPercentage;
+  const theTip = (total * percent) / 100;
+  res.send(`<h1>${theTip}</h1>`);
 });
 
 app.listen(3000, () => {
