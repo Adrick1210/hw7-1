052ecedeb4595bffa37a99a8e0607bbcb53cb9b0
Christian Alexander <christian.alexanderato@gmail.com>
2023-12-14 17:40:02 -0800

    magic 8 ball express app created

--- server.js
+++ server.js
@@ -22,6 +22,41 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
   res.send(`<h1>${theTip}</h1>`);
 });
 
+//=================================
+// Magic 8 Ball
+//=================================
+
+const answer = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+
+app.get("/magic/:question", (req, res) => {
+  const question = req.params.question;
+
+  const ranRes = answer[Math.floor(Math.random() * answer.length)]
+  
+  res.send(`<h1>${question}?</h1> <h2>${ranRes}</h2>`);
+});
+
 app.listen(3000, () => {
   console.log("I am working");
 });
